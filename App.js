fc03d2773517ee49f26eb2c86b8f681a29ccb9f7
Manuel Šarić <manujelko@gmail.com>
2023-02-20 22:30:03 +0100

    Working with core components

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View>
+        <Text>Another piece of text!</Text>
+      </View>
       <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <Button title="Tap me!" />
     </View>
   );
 }
@@ -13,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
